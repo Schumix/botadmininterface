1692de91d6b63564799e353d14ea56b158599b00
Megax <megax@megaxx.info>
2012-03-17 19:52:32 +0100

    * TODOhoz hozzáadva egy hiba.

---
+++ Schumix2/head.php
@@ -0,0 +1,10 @@
+<?php
+function head()
+{
+	echo '<head>
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<link rel="stylesheet" href="css/superfish.css" type="text/css" />
+		<script src="./js/superfish.js" type="text/javascript"></script>
+	      </head>';
+}
+?>
