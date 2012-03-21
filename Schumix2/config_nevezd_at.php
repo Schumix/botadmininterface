0fa99e3f6b0b19970ce1a459933b7335b3c309da
Megax <megax@megaxx.info>
2012-03-21 18:40:03 +0100

    * gitignore

---
+++ Schumix2/config_nevezd_at.php
@@ -0,0 +1,8 @@
+<?php
+
+$database_Hostname = "server2.yeahunter.hu"; // Host name
+$database_Username = "bazd";               // Mysql username
+$database_Password = "meg";              // Mysql password
+$database_Name     = "ircbot_schumacher";    // Database name
+
+?>
